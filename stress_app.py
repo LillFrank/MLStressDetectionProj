9a2cda832dd3aea86348fbd4712402a01e0cc920
frank.lilla <frank.lilla@stud.uni-obuda.hu>
2023-12-12 22:24:37 +0100

    tabs

--- stress_app.py
+++ stress_app.py
@@ -36,10 +36,48 @@ decision_tree_cl.fit(x_train, y_train)
 y_predict = decision_tree_cl.predict(x_test)
 
 
-st.write("Human Stress Detection in and through Sleep")
+st.title("Human Stress Detection in and through Sleep")
 st.divider()
 
-number = st.number_input("Your snoring rate (45 - 100):", value=None, placeholder="Type a number...")
-st.write('The current number is ', number)
+tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["Snoring rate", "Respiration rate", "Body temperature", "Limb movement", "Blood oxygen", "Eye movement"])
+with tab1:
+   st.header("Snoring rate")
+   st.write("The rate or intensity of snoring during sleep, which could be measured in some unit or scale.")
+
+with tab2:
+   st.header("Respiration rate")
+   st.write("The number of breaths taken per minute during sleep.")
+
+with tab3:
+   st.header("Body temeprature")
+   st.write("The body temperature of the user during sleep, possibly measured in degrees Celsius or Fahrenheit.")
+
+with tab4:
+   st.header("Limb movement")
+   st.write("The rate or intensity of limb movement during sleep, indicating how active or restless the person is.")
+
+with tab5:
+   st.header("Blood oxygen")
+   st.write("The blood oxygen level, which represents the amount of oxygen present in the blood during sleep.")
+
+with tab6:
+   st.header("Eye movement")
+   st.write("The eye movement activity during sleep, which might indicate the Rapid Eye Movement (REM) phase of sleep.")
+
+
+sr = st.number_input("Your snoring rate (45 - 100):", value=None, placeholder="Type a number...")
+st.write('The current number is ', sr)
+
+rr = st.number_input("Your respiration rate (45 - 100):", value=None, placeholder="Type a number...")
+st.write('The current number is ', sr)
+
+bt = st.number_input("Your body temperature rate (45 - 100):", value=None, placeholder="Type a number...")
+st.write('The current number is ', sr)
+
+lm = st.number_input("Your limb movement (45 - 100):", value=None, placeholder="Type a number...")
+st.write('The current number is ', sr)
+
+bo = st.number_input("Your blood oxygen (45 - 100):", value=None, placeholder="Type a number...")
+st.write('The current number is ', sr)
 
 
